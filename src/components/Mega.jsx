eaeb626a29f3d7c3b82158ac8e8e527bd6566862
João Victor <soulima.joao@gmail.com>
2021-09-29 17:04:43 -0300

    componente mega estilizado com styled-components

--- src/components/Mega.jsx
+++ src/components/Mega.jsx
@@ -1,5 +1,81 @@
 import React, { Component } from 'react'
-import './Mega.css'
+import styled, {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+`
+
+const WrapperMega = styled.div`
+    width: 250px;
+    padding: 5px;
+    border: 2px dashed #558b2f;
+
+    .display {
+        width: 235px;
+        margin: auto;
+        margin-bottom: 5px;
+        border-radius: 5px;
+        background-color: #558b2f;
+    }
+
+    .display ul {
+        margin: 0;
+        padding: 10px;
+        display: flex;
+        flex-wrap: wrap;
+        list-style: none;
+        min-height: 55px;
+    }
+
+    .display ul li {
+        margin-bottom: 5px;
+        width: 50px;
+        border-radius: 25px;
+        line-height: 50px;
+        background-color: #f1f8e9;
+        color: #ff3d00;
+        font-weight: bold;
+        text-align: center;
+    }
+
+    li + li {
+        margin-left: 5px;
+    }
+
+    .display ul li:nth-child(4n) + li {
+        margin-left: 0;
+    }
+
+    div.entrada {
+        display: flex;
+        justify-content: space-between;
+        width: 240px;
+        margin: auto;
+    }
+
+    input, button {
+        height: 25px;
+    }
+
+    input {
+        width: 150px;
+    }
+
+    button {
+        align-self: center;
+        box-sizing: content-box;
+        background-color: #558b2f;
+        border: none;
+        text-transform: uppercase;
+        width: 65px;
+        color: #f1f8e9;
+        font-weight: bold;
+        cursor: pointer;
+    }
+`
+
 
 export default class Mega extends Component {
     state = {
@@ -38,15 +114,18 @@ export default class Mega extends Component {
         numeros = numeros.map((numero, i) => <li key={i}>{numero}</li>)
         return (
             <div className="Mega">
-                <div className="display">
-                    <ul>
-                        {numeros}
-                    </ul>
-                </div>
-                <div className="entrada">
-                    <input type="number" value={this.state.quatidade} onChange={e => this.onQuantidade(e.target.value)}/>
-                    <button onClick={e => this.gerar(this.state.quatidade)}>gerar</button>
-                </div>
+                <GlobalStyle />
+                <WrapperMega>
+                    <div className="display">
+                        <ul>
+                            {numeros}
+                        </ul>
+                    </div>
+                    <div className="entrada">
+                        <input type="number" value={this.state.quatidade} onChange={e => this.onQuantidade(e.target.value)}/>
+                        <button onClick={e => this.gerar(this.state.quatidade)}>gerar</button>
+                    </div>
+                </WrapperMega>
             </div>
         )
     }
